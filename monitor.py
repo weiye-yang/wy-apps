481720f3f8df38b1bd4b5b29501cefebd76afa65
weiye-yang <xfjzfzboh@gmail.com>
2023-03-11 16:14:08 +0000

    Expose repeat interval

--- monitor.py
+++ monitor.py
@@ -1,12 +1,18 @@
 import argparse
 import datetime as dt
 import os
+from time import sleep
 
 from client import OpenLDBWSClient, expected_time, minutes_diff
 from get_departure_board import departure_board
 
 
-def monitor(crs_from: str, crs_to: str, scheduled: str) -> None:
+def monitor(
+        crs_from: str,
+        crs_to: str,
+        scheduled: str,
+        repeat_seconds: int,
+) -> None:
     client = OpenLDBWSClient()
 
     while True:
@@ -31,23 +37,28 @@ def monitor(crs_from: str, crs_to: str, scheduled: str) -> None:
         print(f"Expected arrival: {arrival_time}")
 
         print(f"Journey duration: {minutes_diff(departure_time, arrival_time)} minutes")
+        print(f"Sleeping for {repeat_seconds} seconds...")
+        sleep(repeat_seconds)
 
 
-def main(crs_from: str, crs_to: str) -> None:
+def main(crs_from: str, crs_to: str, repeat_seconds: int) -> None:
+    if repeat_seconds < 0:
+        raise ValueError(f"repeat_seconds is {repeat_seconds}, but should be non-negative")
     departure_board(crs_from=crs_from, crs_to=crs_to)
     print("")
     default_time = "10:21"
     scheduled = input(f"Enter scheduled time to monitor as hh:mm [default {default_time}]: ").strip()
     if not scheduled:
         scheduled = default_time
-    monitor(crs_from=crs_from, crs_to=crs_to, scheduled=scheduled)
+    monitor(crs_from=crs_from, crs_to=crs_to, scheduled=scheduled, repeat_seconds=repeat_seconds)
     os.system("pause")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # http://www.railwaycodes.org.uk/crs/crs0.shtm
-    parser.add_argument("--fr", default="MAI")
-    parser.add_argument("--to", default="PAD")
+    parser.add_argument("--crs_from", default="MAI")
+    parser.add_argument("--crs_to", default="PAD")
+    parser.add_argument("--repeat_seconds", type=int, default=0)
     args = parser.parse_args()
-    main(crs_from=args.fr, crs_to=args.to)
+    main(crs_from=args.crs_from, crs_to=args.crs_to, repeat_seconds=args.repeat_seconds)
