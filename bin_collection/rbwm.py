d1afd96fc8238bce07c25b1af56054e3789acde1
weiye-yang <xfjzfzboh@gmail.com>
2025-01-18 12:42:31 +0000

    Update message and notify

--- bin_collection/rbwm.py
+++ bin_collection/rbwm.py
@@ -2,12 +2,13 @@ import argparse
 import datetime as dt
 from collections import defaultdict
 
-import dateparser
 import requests
 from bs4 import BeautifulSoup
+from dateutil.parser import parse
 from dateutil.relativedelta import relativedelta
 
-from settings import URPN
+from notifications import pushover_notify
+from settings import URPN_DEFAULT
 
 
 def main(urpn: str) -> None:
@@ -22,19 +23,22 @@ def main(urpn: str) -> None:
         row = [tag.get_text() for tag in tr.find_all("td")]
         if len(row) != 2:
             raise ValueError(f"Unexpected <tr> {row}")
-        row_date = dateparser.parse(row[1]).date()
+        row_date = parse(row[1]).date()
         results[row_date].add(row[0])
 
     tomorrow = dt.date.today() + relativedelta(days=1)
-    tomorrow_bins = results.get(tomorrow)
-    if tomorrow_bins is None:
-        message = f"No bins collected on {tomorrow}"
+    bins = results.get(tomorrow, set())
+    num_bins = len(bins)
+    if num_bins == 0:
+        message = f"No bin collection on {tomorrow}"
     else:
-        message = f"Bins collected on {tomorrow}: {tomorrow_bins}"
+        bins_str = ", ".join(sorted(bins))
+        message = f"{num_bins} bin collection(s) on {tomorrow}: {bins_str}"
+        pushover_notify(message)
     print(message)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--urpn", default=URPN)
+    parser.add_argument("--urpn", default=URPN_DEFAULT)
     args = parser.parse_args()
     main(args.urpn)
--- settings.py
+++ settings.py
@@ -7,4 +7,4 @@ load_dotenv()  # take environment variables from .env.
 RAIL_TOKEN = os.environ["RAIL_TOKEN"]
 PUSHOVER_TOKEN = os.environ["PUSHOVER_TOKEN"]
 PUSHOVER_USER = os.environ["PUSHOVER_USER"]
-URPN = os.environ.get("URPN")
+URPN_DEFAULT = os.environ.get("URPN_DEFAULT")
